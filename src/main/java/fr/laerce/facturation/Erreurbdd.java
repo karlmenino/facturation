fdc60a90879d2b71798dc3225dee760b33abbb94
karlmenino <karl.menino@gmail.com>
2018-11-19 15:55:24 +0100

    tp prof nonfini fin du cours

---
+++ src/main/java/fr/laerce/facturation/Erreurbdd.java
@@ -0,0 +1,23 @@
+package fr.laerce.facturation;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.LogRecord;
+
+public class Erreurbdd extends HttpServlet implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
+
+
---
+++ src/main/webapp/WEB-INF/jsp/erreurbdd.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: CDI
+  Date: 19/11/2018
+  Time: 15:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>error</title>
+</head>
+<body>
+<h1>erreur connection BDD</h1>
+
+</body>
+</html>
