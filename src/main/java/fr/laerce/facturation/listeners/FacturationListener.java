161952d23a50ceb769dca2e2fb53f11d12a70b2a
karlmenino <karl.menino@gmail.com>
2018-11-19 11:18:52 +0100

    correction prof FacturationListener

--- src/main/java/fr/laerce/facturation/listeners/FacturationListener.java
+++ src/main/java/fr/laerce/facturation/listeners/FacturationListener.java
@@ -1,9 +1,6 @@
 package fr.laerce.facturation.listeners;
 
-import javax.servlet.ServletContextAttributeEvent;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,14 +12,21 @@ public class FacturationListener implements ServletContextListener {
     Connection conn;
 
     public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        String url= context.getInitParameter("url");
+        String user= context.getInitParameter("user");
+        String password= context.getInitParameter("password");
+        String port= context.getInitParameter("port");
+        String bdd= context.getInitParameter("bdd");
+        String filename =url+bdd;
             try {
                 Class.forName("org.postgresql.Driver");
                 Properties props = new Properties();
-                props.setProperty("user", "postgres");
-                props.setProperty("password", "secret");
-                conn = DriverManager.getConnection("jdbc:postgresql://192.168.99.100/exemple", props);
+                props.setProperty("user", user);
+                props.setProperty("password", password);
+                conn = DriverManager.getConnection(filename, props);
 //            conn = DriverManager.getConnection("jdbc:postgresql://localhost/exemple", props);
-                sce.getServletContext().setAttribute("conn", conn);
+                context.setAttribute("conn", conn);
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
                 System.out.println(e.getMessage());
@@ -42,10 +46,5 @@ public class FacturationListener implements ServletContextListener {
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
-    }
-
-
-    public Connection getConn() {
-        return conn;
     }
 }
