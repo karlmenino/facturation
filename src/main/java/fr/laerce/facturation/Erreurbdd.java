1ea774d23157c45312be39507fddd72ee3f3d4cc
karlmenino <karl.menino@gmail.com>
2018-11-19 15:59:03 +0100

    tp prof nonfini fin du cours

--- src/main/java/fr/laerce/facturation/Erreurbdd.java
+++ src/main/java/fr/laerce/facturation/Erreurbdd.java
@@ -5,18 +5,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.logging.LogRecord;
 
 public class Erreurbdd extends HttpServlet implements Filter {
-
+Connection conn;
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        conn = (Connection) getServletContext ().getAttribute ("conn");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
+        try {
+            if (conn == null) {
+
+            }
+
+        } catch () {
+        }
     }
 }
 
